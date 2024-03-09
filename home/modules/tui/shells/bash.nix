4c28613a62deb4b2c8ee305546efdd780c35621f
Felix Schausberger <fel.schausberger@gmail.com>
2024-03-09 12:26:30 +0100

    Add bash

--- home/modules/tui/shells/bash.nix
+++ home/modules/tui/shells/bash.nix
@@ -1,12 +1,15 @@
-# { pkgs, ... }:
-{
-  # programs.bash = {
-  #   interactiveShellInit = ''
-  #     if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-  #     then
-  #       shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-  #       exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-  #     fi
-  #   '';
-  # };
+{pkgs, ...}: {
+  programs.bash = {
+    bashrcExtra = ''
+      if (tty) == "/dev/tty1" { Hyprland };
+    '';
+
+    initExtra = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
 }
